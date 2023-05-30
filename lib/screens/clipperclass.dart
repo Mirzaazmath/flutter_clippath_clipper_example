fe73f09c20bdfc6eb60a119910fbd6411836badc
“Mirza” <mirza_azmathullah@cognerium.com>
2023-05-30 12:41:30 +0530

    sixth commit

--- lib/screens/clipperclass.dart
+++ lib/screens/clipperclass.dart
@@ -13,9 +13,9 @@ class MyClipper extends CustomClipper<Path>{
   Path getClip(Size size) {
 
    Path path= Path();
-   path.lineTo(0,0);
-   path.lineTo(size.width/2, size.height);
-   path.lineTo(size.width, 0);
+  path.moveTo( 0,size.height/2);
+  path.lineTo(size.width, size.height);
+  path.lineTo(size.width, 0);
    path.close();
    return path;
   }
